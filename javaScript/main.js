741e9fe1bd5a32c314ae1dcb4effe5c62633d9b3
beso768 <75362419+beso768@users.noreply.github.com>
2021-02-24 08:22:29 -0800

    added images,sections

---
+++ javaScript/main.js
@@ -0,0 +1,9 @@
+// var container = document.getElementById("container") ;
+// var loader = document.getElementById("loader") ;
+// window.addEventListener("load", function () {
+//     setTimeout(function(){
+//         console.log("no"), 3000
+//     })
+// }) ;
+
+setTimeout(console.log("yes"),3000)
