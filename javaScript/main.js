531fcab286fcb26a3fdf651920418a6c0487d7a2
beso768 <75362419+beso768@users.noreply.github.com>
2021-02-26 09:40:25 -0800

    add responsive

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,9 +1,20 @@
-// var container = document.getElementById("container") ;
-// var loader = document.getElementById("loader") ;
-// window.addEventListener("load", function () {
-//     setTimeout(function(){
-//         console.log("no"), 3000
-//     })
-// }) ;
+var container = document.getElementById("loaderDiv") ;
+var loader = document.getElementById("loader") ;
+function load(){
+    setTimeout(function (){
+        loader.style.display = "none" ;
+        container.style.display = "block" ;
+    },2000)
+}
+window.addEventListener("load", load()) ;
 
-setTimeout(console.log("yes"),3000)
+var burger = document.getElementById("burgerIcon") ;
+var menu = document.getElementById("burgerMenu");
+burgerIcon.addEventListener("click" , function(){
+    menu.style.left = "0" ;
+})
+
+var back = document.getElementById("Back") ;
+back.addEventListener("click" , function(){
+    menu.style.left = "-100%"
+})
